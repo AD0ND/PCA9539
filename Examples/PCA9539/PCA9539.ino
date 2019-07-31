56395372b4cf433e33d914c036c2ee6f8b1420e4
Shawn McClintock <kd6oji@gmail.com>
2019-07-30 20:26:07 -0500

    Update PCA9539.ino

--- Examples/PCA9539/PCA9539.ino
+++ Examples/PCA9539/PCA9539.ino
@@ -7,7 +7,7 @@
 #include "PCA9539.h"
 #include "Wire.h"
 
-PCA9539 ioport(0x74); // Base address starts at 0x74 for A0 = L and A1 = L
+PCA9539 ioport(0x20); // Base address starts at 0x74 for A0 = L and A1 = L
 
 //Base address for PCA9539 devices 
 //Address   A1    A0
@@ -26,15 +26,42 @@ PCA9539 ioport(0x74); // Base address starts at 0x74 for A0 = L and A1 = L
 void setup()
 {
   //
-  // set first 14 pins to output
+  // set pins to output
   //
-  for (uint8_t i = 0; i < 14; i++){
-    ioport.pinMode(i, OUTPUT);
-  }
-  //
-  // set pin 15  (=digital 15) to Input
-  //
-  ioport.pinMode(ED14, INPUT);
+  
+    ioport.pinMode(EA0, OUTPUT);
+    ioport.pinMode(EA1, OUTPUT);
+    ioport.pinMode(EA2, OUTPUT);
+    ioport.pinMode(EA3, OUTPUT);
+    ioport.pinMode(EA4, OUTPUT);
+    ioport.pinMode(EA5, OUTPUT);
+    ioport.pinMode(EA6, OUTPUT);
+    ioport.pinMode(EA7, OUTPUT);
+    ioport.pinMode(EB0, OUTPUT);
+    ioport.pinMode(EB1, OUTPUT);
+    ioport.pinMode(EB2, OUTPUT);
+    ioport.pinMode(EB3, OUTPUT);
+    ioport.pinMode(EB4, OUTPUT);
+    ioport.pinMode(EB5, OUTPUT);
+    ioport.pinMode(EB6, OUTPUT);
+    ioport.pinMode(EB7, OUTPUT);
+    ioport.digitalWrite(EA0, HIGH);
+  ioport.digitalWrite(EA1, HIGH);
+  ioport.digitalWrite(EA2, HIGH);
+  ioport.digitalWrite(EA3, HIGH);
+  ioport.digitalWrite(EA4, HIGH);
+  ioport.digitalWrite(EA5, HIGH);
+  ioport.digitalWrite(EA6, HIGH);
+  ioport.digitalWrite(EA7, HIGH);
+  ioport.digitalWrite(EB0, LOW);
+  ioport.digitalWrite(EB1, LOW);
+  ioport.digitalWrite(EB2, LOW);
+  ioport.digitalWrite(EB3, LOW);
+  ioport.digitalWrite(EB4, LOW);
+  ioport.digitalWrite(EB5, LOW);
+  ioport.digitalWrite(EB6, LOW);
+  ioport.digitalWrite(EB7, LOW);
+  
 }
 
 /**
@@ -43,23 +70,46 @@ void setup()
  */
 void loop()
 {
-  //
-  // check if button is LOW
-  //
-  if (ioport.digitalRead(ED14) == LOW) {
-    //
-    // turn all 14 leds on
-    //
-    for (uint8_t i = 0; i < 14; i++){
-      ioport.digitalWrite(i, HIGH);
-      delay(50);
-    }
-    //
-    // turn all 14 leds off
-    //
-    for (uint8_t i = 0; i < 14; i++){
-      ioport.digitalWrite(i, LOW);
-      delay(50);
-    }
-  }
+  ioport.digitalWrite(EB0, HIGH);
+  ioport.digitalWrite(EA0, LOW);
+  ioport.digitalWrite(EA7, HIGH);
+  ioport.digitalWrite(EB7, LOW);
+  delay(100);
+  ioport.digitalWrite(EB1, HIGH);
+  ioport.digitalWrite(EA1, LOW);
+  ioport.digitalWrite(EA0, HIGH);
+  ioport.digitalWrite(EB0, LOW);
+  delay(100);
+  ioport.digitalWrite(EB2, HIGH);
+  ioport.digitalWrite(EA2, LOW);
+  ioport.digitalWrite(EA1, HIGH);
+  ioport.digitalWrite(EB1, LOW);
+  delay(100);
+  ioport.digitalWrite(EB3, HIGH);
+  ioport.digitalWrite(EA3, LOW);
+  ioport.digitalWrite(EA2, HIGH);
+  ioport.digitalWrite(EB2, LOW);
+  delay(100);
+  ioport.digitalWrite(EB4, HIGH);
+  ioport.digitalWrite(EA4, LOW);
+  ioport.digitalWrite(EA3, HIGH);
+  ioport.digitalWrite(EB3, LOW);
+  delay(100);
+  ioport.digitalWrite(EB5, HIGH);
+  ioport.digitalWrite(EA5, LOW);
+  ioport.digitalWrite(EA4, HIGH);
+  ioport.digitalWrite(EB4, LOW);
+  delay(100);
+  ioport.digitalWrite(EB6, HIGH);
+  ioport.digitalWrite(EA6, LOW);
+  ioport.digitalWrite(EA5, HIGH);
+  ioport.digitalWrite(EB5, LOW);
+  delay(100);
+  ioport.digitalWrite(EB7, HIGH);
+  ioport.digitalWrite(EA7, LOW);
+  ioport.digitalWrite(EA6, HIGH);
+  ioport.digitalWrite(EB6, LOW);
+  delay(100);  
+  
+ 
 }
